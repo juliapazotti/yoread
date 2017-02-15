44c7403129660125ff2b55e12d25346e2d5ffce9
juliapazotti <juliapazotti7@gmail.com>
2017-02-15 12:11:47 -0200

    Primeiro commit
    yoread

---
+++ yoreadsolution/yoread/inicial.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace yoread
+{
+    public partial class inicial : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
