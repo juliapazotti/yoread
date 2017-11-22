bd53b939d40a8d0a43a55ebb180a01ff6b50f309
Geleia <luizcanf@gmail.com>
2017-11-22 16:38:58 -0200

    Carregamento dinâmico das publicações

--- inicial.php
+++ inicial.php
@@ -15,9 +15,9 @@
 		<link rel="stylesheet" href="styleinicial.css" />
 		<link href="https://fonts.googleapis.com/css?family=Cedarville+Cursive" rel="stylesheet">
 	</head>
-	
+
 	<body>
-		
+
 		<!-- topo -->
 		<div id = "topo">
 			<div id = "logo">
@@ -25,15 +25,15 @@
 			</div>
 			<div id = "divavatar">
 				<div id = "posiavatar">
-					<a class = "avatar" href="perfil.html" ><img src = "avatar.png" title="Perfil" border="none" /></a> 
+					<a class = "avatar" href="perfil.html" ><img src = "avatar.png" title="Perfil" border="none" /></a>
 				</div>
 			</div>
 			<div id = "divpub">
 				<div id = "posipublicon">
-					<a class = "publicon" href="publicar.html" ><img src = "publicon.png" title="Publicar" border="none" /></a> 
+					<a class = "publicon" href="publicar.html" ><img src = "publicon.png" title="Publicar" border="none" /></a>
 				</div>
 			</div>
-			
+
 			<div style="float: right; margin: -20px 20px auto;">
 				<a href= "perfil.html">
 					<?php echo $username ?>
@@ -41,44 +41,43 @@
 				&nbsp;
 				<a href= "php\logout.php">
 					Logout
-				</a>					
+				</a>
 			</div>
 		</div>
-		
+
 		<!-- meio -->
-		<div class = "quadro1">
-				<div class = "textoconf">
-					<h1 class = "titulosinopse">A dona do tempo</h1>
-					<p class = "sinopse">Catarina é uma menina muito normal, só que um dia ela descobriu que tinha poderes mágicos, e podia controlar o tempo. Então, ela conhece Ian e descobre que ele vai mudar a vida dela para sempre. No decorrer da história você vai se deparar com criaturas mágicas, novos mundos, amor, ódio, fantasia, pensamentos filosóficos que mudarão a sua vida. <a class = "lermais" href="publicacao.html">Ler mais...</a></p>
-					<a class = "autor" href="#">Diana Mastalli</a>
-				</div>
-				<div class = "posicon">
-					<a class = "icon" href="#" ><img src = "add.png" title="favoritar" border="none" /></a> 
-				</div>
-		</div>
-		
-		<div class = "quadro2">
-				<div class = "textoconf">
-					<h1 class = "titulosinopse">A escolhida</h1>
-					<p class = "sinopse"> O jogo começou. Uma arena, várias mulheres. Uma nova participante. A escolhida, que acabará fazendo suas próprias regras, mudando tudo o que eles já foram capazes de acreditar um dia. O desejo de vingança corria por suas veias. <a class = "lermais" href="publicacao.html">Ler mais...</a></p>
-					<a class = "autor" href="#">Yara Rangel</a>
-				</div>
-				<div class = "posicon">
-					<a class = "icon" href="#" ><img src = "add.png" title="favoritar" border="none" /></a> 
-				</div>
-		</div>
-		
-		<div class = "quadro3">
-				<div class = "textoconf">
-					<h1 class = "titulosinopse">A resposta</h1>
-					<p class = "sinopse">“Não é apenas uma herança, se trata de algo maior. Tão grandioso que me trouxe de volta algo que achava que não precisava mais. Me trouxe você. O engraçado é que, como um ato em vão, tentei desesperadamente tirá-lo de minha cabeça. Creio que há certas coisas que não somos capaz de compreender. <a class = "lermais" href="publicacao.html">Ler mais...</a></p>
-					<a class = "autor" href="#">Sophia Sanches</a>
-				</div>
-				<div class = "posicon">
-					<a class = "icon" href="#" ><img src = "add.png" title="favoritar" border="none" /></a> 
-				</div>
-		</div>
-		
+		<?php
+		require_once("../config.php");
+		$config = new Config();
+		$conexao = $config->conectaBanco();
+
+		$query = "select titulo, sinopse, id_user, cod_publicacao from yoread.publicacao";
+
+		$result = mysqli_query($conexao, $query) or die('Invalid query: ' . $conexao->error);
+
+		$num = 0;
+		if ($result->num_rows > 0) {
+    	// output data of each row
+	    while($row = $result->fetch_assoc()) {
+				$num = $num + 1;
+				echo "<div class = \"quadro".$num."\">";
+				echo "		<div class = \"textoconf\">";
+				echo "			<h1 class = \"titulosinopse\">".$row["titulo"]."</h1>";
+				echo "			<p class = \"sinopse\">".$row["sinopse"]."";
+				echo "			<a class = \"lermais\" href=\"publicacao.php?id".$row["cod_publicacao"]."\">Ler mais...</a></p>";
+				echo "			<a class = \"autor\" href=\"#\">".$row["id_user"]."</a>";
+				echo "		</div>";
+				echo "		<div class = \"posicon\">";
+				echo "			<a class = \"icon\" href=\"#\" ><img src = \"add.png\" title=\"favoritar\" border=\"none\" /></a>";
+				echo "		</div>";
+				echo "</div>";
+	    }
+		} else {
+			echo "<p>Nenhuma sinopse encontrada.</p>";
+		}
+		$conexao->close();
+		?>
+
 		<div class = "quadro4">
 				<div class = "textoconf">
 					<h1 class = "titulosinopse">O poder de amar</h1>
@@ -86,10 +85,10 @@
 					<a class = "autor" href="#">Otávio Castanho</a>
 				</div>
 				<div class = "posicon">
-					<a class = "icon" href="#" ><img src = "add.png" title="favoritar" border="none" /></a> 
+					<a class = "icon" href="#" ><img src = "add.png" title="favoritar" border="none" /></a>
 				</div>
 		</div>
-		
+
 		<div class = "quadro5">
 				<div class = "textoconf">
 					<h1 class = "titulosinopse">O diário de Lauren</h1>
@@ -97,10 +96,10 @@
 					<a class = "autor" href="#">Olivia Pinheiro</a>
 				</div>
 				<div class = "posicon">
-					<a class = "icon" href="#" ><img src = "add.png" title="favoritar" border="none" /></a> 
+					<a class = "icon" href="#" ><img src = "add.png" title="favoritar" border="none" /></a>
 				</div>
 		</div>
-		
+
 		<div class = "quadro6">
 				<div class = "textoconf">
 					<h1 class = "titulosinopse">Desculpas</h1>
@@ -108,9 +107,9 @@
 					<a class = "autor" href="#">Edinaldo Gaspar</a>
 				</div>
 				<div class = "posicon">
-					<a class = "icon" href="#" ><img src = "add.png" title="favoritar" border="none" /></a> 
+					<a class = "icon" href="#" ><img src = "add.png" title="favoritar" border="none" /></a>
 				</div>
 		</div>
-		
+
 	</body>
 </html>
